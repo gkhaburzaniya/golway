ada3773ea135d3c9a6e92cfa59b472d14952e66c
George Khaburzaniya <george.khaburzaniya@gmail.com>
2023-12-08 17:47:12 -0800

    Refactor..., somewhat poorly.

--- player.py
+++ player.py
@@ -1,4 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from character_creator import steps
+
+from pygame import Rect
 
 import pygame as pg
 import constants
@@ -7,7 +10,8 @@ import constants
 @dataclass(eq=False)
 class Player:
 
-    creation_step = 0
+    buttons: list[Rect] = field(default_factory=list)
+    creation_step: int = 0
 
     type: str = ""
     name: str = ""
@@ -24,72 +28,9 @@ class Player:
     quickness: int = -10
 
     def create_character(self, surface):
-        font = pg.font.SysFont("Arial", 20)
-
-        if self.creation_step == 0:
-            magus_text = font.render("Magus", True, "blue")
-            self.magus_button = pg.draw.rect(
-                surface,
-                "dark grey",
-                (0, 30, magus_text.get_width() + 5,
-                 magus_text.get_height() + 5),
-            )
-            magus_text_rect = magus_text.get_rect(
-                center=self.magus_button.center)
-
-            companion_text = font.render("Companion", True, "red")
-            self.companion_button = pg.draw.rect(
-                surface,
-                "dark grey",
-                (self.magus_button.right + 25, 30,
-                 companion_text.get_width() + 5,
-                 companion_text.get_height() + 5),
-            )
-            companion_text_rect = companion_text.get_rect(
-                center=self.companion_button.center)
-
-            grog_text = font.render("Grog", True, "white")
-            self.grog_button = pg.draw.rect(
-                surface,
-                "dark grey",
-                (self.companion_button.right + 25, 30,
-                 grog_text.get_width() + 5, grog_text.get_height() + 5),
-            )
-            grog_text_rect = grog_text.get_rect(center=self.grog_button.center)
-            surface.blit(font.render("What Kind of Character are You?", True,
-                                     "white"), (0, 0))
-            surface.blit(magus_text, magus_text_rect)
-            surface.blit(companion_text, companion_text_rect)
-            surface.blit(grog_text, grog_text_rect)
+        for step in steps:
+            self.buttons = step(self, surface, self.buttons)
             self.creation_step += 1
-
-        mouse_x, mouse_y = pg.mouse.get_pos()
-        mouse_x, mouse_y = mouse_x - constants.SURFACE_X, mouse_y - constants.SURFACE_Y
-        if pg.mouse.get_pressed()[0] == 1:
-            if self.magus_button.collidepoint(mouse_x, mouse_y):
-                self.type = "Magus"
-
-                surface.fill("black")
-                surface.blit(
-                    font.render("You are a Magus", True, "white"),
-                    (60, 0))
-                return True
-            if self.companion_button.collidepoint(mouse_x, mouse_y):
-                self.type = "Companion"
-
-                surface.fill("black")
-                surface.blit(
-                    font.render("You are a Companion", True, "white"),
-                    (60, 0))
-                return True
-            if self.grog_button.collidepoint(mouse_x, mouse_y):
-                self.type = "Grog"
-
-                surface.fill("black")
-                surface.blit(
-                    font.render("You are a Grog", True, "white"),
-                    (60, 0))
-                return True
 
     def change_intelligence(self, change):
         self.intelligence += change
